f25cfdd8f9d879589cd787050e118bc475159165
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-07-02 17:28:03 -0700

    added foodscaner component and install nvm to change node version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import BarcodeScanner from "react-qr-barcode-scanner";
+ 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [foodCode, setFoodCode] = useState(0)
 
   return (
     <>
+      <BarcodeScanner
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) setFoodCode(result.text);
+          else console.log(err); 
+        }}
+      />
 
+      <div>{foodCode}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
