40dd4fa8a1c6dcf6689dd7ea53b4ed0f532218f7
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-07-11 16:47:19 -0700

    cranked out the basic outline for the card analytics section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,59 +5,57 @@ import BarcodeScanner from "react-qr-barcode-scanner";
 
 import Popup from './components/popup';
 import StartButton from './components/startButton';
-
+import CardData from './components/cardData';
 
 function App() {
   const [foodCode, setFoodCode] = useState(0)
 
   return (
-    
-    <div className='scanner d-flex flex-column justify-content-center align-items-center'>
-      <div className='container'>
+    <>
+      <div className='scanner d-flex flex-column justify-content-center align-items-center'>
+        <div className='container'>
 
-        <div className='row gap-4'>
-          <div className='d-flex justify-content-center flex-row col-12'>
-            <div className='scanner_container d-flex justify-content-center align-items-center flex-column'>
+          <div className='row gap-4'>
+            <div className='d-flex justify-content-center flex-row col-12'>
+              <div className='scanner_container d-flex justify-content-center align-items-center flex-column'>
 
-              <div className='text scanner_text'>Scan Barcode Below</div>
+                <div className='text scanner_text'>Scan Barcode Below</div>
 
-              <div className='camera_wrapper px-2'>
-                <BarcodeScanner
-                  onUpdate={(err, result) => {
-                    if (result) setFoodCode(result.text);
-                    else console.log(err); 
-                  }}
-                />
+                <div className='camera_wrapper px-2'>
+                  <BarcodeScanner
+                    onUpdate={(err, result) => {
+                      if (result) setFoodCode(result.text);
+                      else console.log(err); 
+                    }}
+                  />
+                </div>
               </div>
+            </div>
+
+            <div className='col-12 d-flex justify-content-center'>
+                <StartButton
+                  width='31.25rem'
+                  height='2.5rem'
+                >Extract Barcode</StartButton>
             </div>
           </div>
 
-          <div className='col-12 d-flex justify-content-center'>
-              <StartButton
-                width='31.25rem'
-                height='2.5rem'
-              >Extract Barcode</StartButton>
-          </div>
         </div>
-
-
-        
       </div>
-     {/* <div className='row'>
+      
 
-        <BarcodeScanner
-          className='col-auto'
-          width={500}
-          height={500}
-          onUpdate={(err, result) => {
-            if (result) setFoodCode(result.text);
-            else console.log(err); 
-          }}
-        />
-      </div> */}
-        
+      <div className='data d-flex flex-column justify-content-center align-items-center'>
+            <div className='container d-flex justify-content-center align-items-center'>
 
-    </div>
+              <div className='row'>
+                  <CardData />
+              </div>
+
+            </div>
+      </div>
+    </>
+
+
   );
 }
 
---
+++ frontend/src/components/cardData.jsx
@@ -0,0 +1,14 @@
+import './cardData.css';
+
+
+function CardData() {
+    return (
+      <div className='cardContainer'>
+
+
+      </div>  
+    );
+}
+
+
+export default CardData;
