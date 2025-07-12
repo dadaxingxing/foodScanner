c6d0edeebfebd3c344bba53ea183a0bc50bbf800
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-07-11 17:42:22 -0700

    added a header to the top of the data card

--- frontend/src/components/cardData.jsx
+++ frontend/src/components/cardData.jsx
@@ -3,7 +3,10 @@ import './cardData.css';
 
 function CardData() {
     return (
-      <div className='cardContainer'>
+      <div className='cardContainer px-0'>
+          <div className='cardTop d-flex justify-content-center align-items-center'>
+            <div className='text'>Nutrition Info</div>
+          </div>
 
 
       </div>  
