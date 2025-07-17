d74c228970b896d342ff64307dd50aa62c8d8e11
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-07-17 15:35:02 -0700

    centered cardData, need to apply more styling

--- frontend/src/components/cardData.jsx
+++ frontend/src/components/cardData.jsx
@@ -8,32 +8,35 @@ function CardData({ data }) {
             <div className='text'>Nutrition Info</div>
           </div>
 
+          <div className='cardBottom d-flex justify-content-center align-items-center flex-column'>
 
-          {data && (
-            <div className='cardContent'>
-            
-              {data?.food_name && (
-                <div>😋{data.food_name}</div>
-              )}
+            {data && (
+              <div>
               
-              {data?.g100 && (
-                <div>⚖️ Total 100g calories
-                  <div>🥩 {data.g100.protein_cal_100g} protein</div>
-                  <div>🍙 {data.g100.carb_cal_100g} carb</div>
-                  <div>🧈 {data.g100.fat_cal_100} fats</div>
-                </div>
-              )}
+                {data?.food_name && (
+                  <div className='foodName'>😋 {data.food_name}</div>
+                )}
+                
+                {data?.g100 && (
+                  <div>⚖️ Total 100g calories
+                    <div>🥩 {data.g100.protein_cal_100g || '0'} kcal protein</div>
+                    <div>🍙 {data.g100.carb_cal_100g || '0'} kcal carb</div>
+                    <div>🧈 {data.g100.fat_cal_100g || '0'} kcal fats</div>
+                  </div>
+                )}
 
-              {data?.total && (
-                <div>🍲 {data.total.total_cal}
-                  <div>🍖 {data.total.protein_cal}</div>
-                  <div>🥙 {data.total.carb_cal}</div>
-                  <div>🐖 {data.total.fat_cal}</div>
-                </div>
-              )}
+                {data?.total && (
+                  <div>🍲 Total calories {data.total.total_cal} kcal 
+                    <div>🍖 {data.total.protein_cal || '0'} kal protein</div>
+                    <div>🥙 {data.total.carb_cal || '0'} kal carb</div>
+                    <div>🐖 {data.total.fat_cal || '0'} kal fat</div>
+                  </div>
+                )}
 
-            </div>
-          )}
+              </div>
+            )}
+
+          </div>
       </div>  
     );
 }
